c0e90505b21ec2dcc4919b138f1c960d8ade17bc
Romain Reignier <romain.reignier@exail.com>
2025-09-02 19:59:44 +0200

    Add TopicDiagnostic

--- src/publisher.rs
+++ src/publisher.rs
@@ -1,7 +1,9 @@
 use std::sync::{Arc, Mutex};
 
 use crate::diagnostic_updater::{CompositeDiagnosticTask, DiagnosticTask, Updater};
-use crate::update_functions::{FrequencyStatus, FrequencyStatusParam};
+use crate::update_functions::{
+    FrequencyStatus, FrequencyStatusParam, TimeStampStatus, TimeStampStatusParam,
+};
 
 pub struct HeaderlessTopicDiagnostic {
     task: Arc<Mutex<CompositeDiagnosticTask>>,
@@ -18,6 +20,32 @@ impl<'a> HeaderlessTopicDiagnostic {
         S: Into<String>,
     {
         let frequency_status = Arc::new(Mutex::new(FrequencyStatus::new(freq_param)?));
+        HeaderlessTopicDiagnostic::new_internal(name, diag, frequency_status)
+    }
+
+    pub fn with_clock<S>(
+        name: S,
+        diag: &mut Updater,
+        freq_param: FrequencyStatusParam,
+        clock: rclrs::Clock,
+    ) -> Result<Self, rclrs::RclrsError>
+    where
+        S: Into<String>,
+    {
+        let frequency_status = Arc::new(Mutex::new(
+            FrequencyStatus::new(freq_param)?.with_clock(clock),
+        ));
+        HeaderlessTopicDiagnostic::new_internal(name, diag, frequency_status)
+    }
+
+    fn new_internal<S>(
+        name: S,
+        diag: &mut Updater,
+        frequency_status: Arc<Mutex<FrequencyStatus>>,
+    ) -> Result<Self, rclrs::RclrsError>
+    where
+        S: Into<String>,
+    {
         let task = Arc::new(Mutex::new(CompositeDiagnosticTask::new(name)));
         let instance = Self {
             task: task.clone(),
@@ -41,5 +69,75 @@ impl<'a> HeaderlessTopicDiagnostic {
         T: DiagnosticTask + 'static + Send + Sync,
     {
         self.task.lock().unwrap().add_task(task);
+    }
+}
+
+struct TopicDiagnostic {
+    topic_diag: HeaderlessTopicDiagnostic,
+    timestamp_status: Arc<Mutex<TimeStampStatus>>,
+}
+
+impl TopicDiagnostic {
+    pub fn new<S>(
+        name: S,
+        diag: &mut Updater,
+        freq_param: FrequencyStatusParam,
+        time_param: TimeStampStatusParam,
+    ) -> Result<Self, rclrs::RclrsError>
+    where
+        S: Into<String>,
+    {
+        let timestamp_status = Arc::new(Mutex::new(TimeStampStatus::new(time_param)?));
+        let topic_diag = HeaderlessTopicDiagnostic::new(name, diag, freq_param)?;
+        topic_diag
+            .task
+            .lock()
+            .unwrap()
+            .add_task(timestamp_status.clone());
+        Ok(Self {
+            topic_diag,
+            timestamp_status,
+        })
+    }
+
+    pub fn with_clock<S>(
+        name: S,
+        diag: &mut Updater,
+        freq_param: FrequencyStatusParam,
+        time_param: TimeStampStatusParam,
+        clock: rclrs::Clock,
+    ) -> Result<Self, rclrs::RclrsError>
+    where
+        S: Into<String>,
+    {
+        let timestamp_status = Arc::new(Mutex::new(
+            TimeStampStatus::new(time_param)?.with_clock(clock.clone()),
+        ));
+        let topic_diag = HeaderlessTopicDiagnostic::with_clock(name, diag, freq_param, clock)?;
+        topic_diag
+            .task
+            .lock()
+            .unwrap()
+            .add_task(timestamp_status.clone());
+        Ok(Self {
+            topic_diag,
+            timestamp_status,
+        })
+    }
+
+    pub fn tick(&mut self, stamp: &rclrs::Time) {
+        self.timestamp_status.lock().unwrap().tick_with_time(stamp);
+        self.topic_diag.tick();
+    }
+
+    pub fn clear_window(&mut self) {
+        self.topic_diag.clear_window();
+    }
+
+    pub fn add_task<T>(&mut self, task: Arc<Mutex<T>>)
+    where
+        T: DiagnosticTask + 'static + Send + Sync,
+    {
+        self.topic_diag.add_task(task);
     }
 }
